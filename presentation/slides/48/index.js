9a15e325c0f05e6d1d70af174f9026e148ee67d4
David A. Lindahl <davidalindahl@gmail.com>
2018-10-10 13:28:51 -0700

    trying new build command

---
+++ presentation/slides/48/index.js
@@ -0,0 +1,15 @@
+// Import React
+import React from "react";
+import { Heading, Slide, Image } from "spectacle";
+
+const picture = require("../../../assets/t-hanks.jpg");
+
+export default (
+  <Slide bgColor="primary">
+    <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+      El Fin
+    </Heading>
+    <Image src={picture} />
+
+  </Slide>
+);
